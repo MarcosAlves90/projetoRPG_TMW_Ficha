5087eb3cc061740c08676adcfba4e559d5ef56ce
Marcos Alves <marcos.lopes26@fatec.sp.gov.br>
2024-08-31 14:59:08 -0300

    :sparkles: feat: update files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/projetoRPG_TMW_Ficha/' // Ajuste para o caminho do seu projeto no GitHub Pages
+  base: '/'
 });
