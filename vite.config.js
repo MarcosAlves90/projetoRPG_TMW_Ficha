2c0df6dc49300c39e6c320a897ca99964a105130
Marcos Alves <marcos.lopes26@fatec.sp.gov.br>
2024-09-09 02:39:40 -0300

    :sparkles: feat: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  define: {
+    'import.meta.env.VITE_APP_FIREBASE_API_KEY': JSON.stringify(import.meta.env.VITE_APP_FIREBASE_API_KEY),
+    'import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN),
+    'import.meta.env.VITE_APP_FIREBASE_PROJECT_ID': JSON.stringify(import.meta.env.VITE_APP_FIREBASE_PROJECT_ID),
+    'import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET': JSON.stringify(import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET),
+    'import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID),
+    'import.meta.env.VITE_APP_FIREBASE_APP_ID': JSON.stringify(import.meta.env.VITE_APP_FIREBASE_APP_ID),
+    'import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID': JSON.stringify(import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID)
+  }
 });
