0a55c3f2b77d853de3c78a26c49e200a6a039d15
MarcosAlves90 <MarcosAlves25@proton.me>
2025-01-04 20:25:33 -0300

    :sparkles: feat: add @vercel/speed-insights for performance monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,14 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {UserProvider} from "./UserContext.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <UserProvider>
                 <App />
+                <SpeedInsights/>
             </UserProvider>
         </BrowserRouter>
     </StrictMode>,
