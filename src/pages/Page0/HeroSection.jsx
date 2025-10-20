a246ab8f844e404d9545c0f8f3067f5e470c0cbd
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-10-20 17:16:17 -0300

    Add HeroSection component and styles

---
+++ src/pages/Page0/HeroSection.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import styles from './HeroSection.module.css';
+
+export default function HeroSection({ title, category, subtitle }) {
+    return (
+        <section className="mainCommon-page-0-section-0">
+            <article className="mainCommon-page-0-section-0-article">
+                <div>
+                    <h2 className={styles.category}>{category}</h2>
+                    <h1 className={styles.title}>{title}</h1>
+                    <p className={styles.subtitle}>
+                        {subtitle}
+                    </p>
+                </div>
+            </article>
+        </section>
+    );
+}
+
+HeroSection.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+};
