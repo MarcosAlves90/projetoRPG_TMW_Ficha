3d9c6effca7b200aef438736fca772e0d0d395ab
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-10-20 16:31:22 -0300

    refactor(MentalWorldSection): move styled-components to CSS module and update component classes

--- src/pages/Page1/MentalWorldSection.jsx
+++ src/pages/Page1/MentalWorldSection.jsx
@@ -1,83 +1,14 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import {TextField, Select, MenuItem, FormControl, InputLabel} from "@mui/material";
-
-const StyledTextField = styled(TextField)`
-    margin-top: 0;
-
-    .MuiInputLabel-root, .MuiInputBase-input {
-        font-family: var(--common-font-family), sans-serif !important;
-    }
-
-    & .MuiFilledInput-root {
-        background-color: var(--background);
-    }
-
-    @media (max-width: 991px) {
-        & .MuiInputBase-input, .MuiInputLabel-root {
-            font-size: 3.5vw;
-        }
-    }
-`;
-
-const StyledFormControl = styled(FormControl)`
-    margin-top: 0;
-
-    .MuiInputLabel-root, .MuiInputBase-input {
-        font-family: var(--common-font-family), sans-serif !important;
-        text-align: left;
-    }
-
-    & .MuiFilledInput-root {
-        background-color: var(--background);
-    }
-
-    @media (max-width: 991px) {
-        & .MuiInputBase-input, .MuiInputLabel-root {
-            font-size: 3.5vw;
-        }
-    }
-`;
-
-const InputsFieldset = styled.fieldset`
-    margin: 1.5rem;
-    display: flex;
-    gap: 2rem;
-
-    @media (max-width: 991px) {
-        flex-direction: column;
-        margin: 1rem;
-        gap: 1rem;
-    }
-`;
-
-const SectionCommon = styled.section`
-    width: 100%;
-    margin-bottom: 1rem;
-
-    .title-2 {
-        width: 100%;
-        margin: 0;
-        color: white;
-        background-color: var(--gray-border-color);
-        padding: 0.5rem;
-        font-size: 1.2rem;
-    }
-    
-    @media (max-width: 991px) {
-        .title-2 {
-            font-size: 4.5vw;
-            padding: 0.8rem 0.5rem;
-        }
-    }
-`;
+import styles from './MentalWorldSection.module.css';
 
 export default function MentalWorldSection({userData, onInputChange}) {
     return (
-        <SectionCommon>
-            <h2 className="mainCommon title-2">Mundo Mental</h2>
-            <InputsFieldset>
-                <StyledTextField
+        <section className={styles.sectionCommon}>
+            <h2 className={`mainCommon ${styles.title2}`}>Mundo Mental</h2>
+            <fieldset className={styles.inputsFieldset}>
+                <TextField
+                    className={styles.styledTextField}
                     variant="filled"
                     label="Nome da Forma"
                     fullWidth
@@ -85,7 +16,7 @@ export default function MentalWorldSection({userData, onInputChange}) {
                     value={userData.nomeF || ''}
                     onChange={onInputChange('nomeF')}
                 />
-                <StyledFormControl variant="filled" fullWidth>
+                <FormControl className={styles.styledFormControl} variant="filled" fullWidth>
                     <InputLabel>Categoria da Forma</InputLabel>
                     <Select
                         value={userData.forma || ''}
@@ -96,8 +27,9 @@ export default function MentalWorldSection({userData, onInputChange}) {
                         <MenuItem value={2}>Fobia</MenuItem>
                         <MenuItem value={3}>Trauma</MenuItem>
                     </Select>
-                </StyledFormControl>
-                <StyledTextField
+                </FormControl>
+                <TextField
+                    className={styles.styledTextField}
                     variant="filled"
                     label="Tipo da Forma"
                     fullWidth
@@ -105,8 +37,8 @@ export default function MentalWorldSection({userData, onInputChange}) {
                     value={userData.tipoF || ''}
                     onChange={onInputChange('tipoF')}
                 />
-            </InputsFieldset>
-        </SectionCommon>
+            </fieldset>
+        </section>
     );
 }
 
