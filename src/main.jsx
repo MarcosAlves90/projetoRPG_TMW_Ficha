c1c3eefe1c489c315b0b439042e7c5b827331d7f
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-10-21 02:56:23 -0300

    chore: remove Bootstrap imports from main entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@popperjs/core/dist/umd/popper.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx'
