7e077702fd3a42a6b6718f3e3401598c73616816
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-11-15 11:59:01 -0300

    feat: Add isLoadingUserData to UserContext provider

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -6,9 +6,17 @@ export const UserContext = createContext();
 export function UserProvider({ children }) {
     const [userData, setUserData] = useState({ nivel: 0 });
     const [user, setUser] = useState(null);
+    const [isLoadingUserData, setIsLoadingUserData] = useState(true);
 
     return (
-        <UserContext.Provider value={useMemo(() => ({ userData, setUserData, user, setUser }), [userData, user])}>
+        <UserContext.Provider value={useMemo(() => ({ 
+            userData, 
+            setUserData, 
+            user, 
+            setUser,
+            isLoadingUserData,
+            setIsLoadingUserData
+        }), [userData, user, isLoadingUserData])}>
             {children}
         </UserContext.Provider>
     );
